b1a873bf9826d6018e6bc6c63a313e592b4df98f
thereapher <gumello.moura@gmail.com>
2025-05-23 17:45:39 -0300

    JS inicial

--- cardapio/js/cardapio.js
+++ cardapio/js/cardapio.js
@@ -1,64 +1,63 @@
+const fundomenu = document.querySelector('.fundomenu');
+const menu = document.querySelector('.menu');
+const fecharMenu = document.querySelector('.fecharmenu');
+const itensPizza = document.querySelector('.pizza');
+const tamanho = document.querySelector('.tamanho');
+const borda = document.querySelector('.borda');
+const Retirar = document.querySelector('.retirar');
+const AddCart = document.querySelector('.addcart');
+const FundoCarrinho = document.querySelector('.fundocarrinho');
+const Carrinho = document.querySelector('.carrinho');
+const FecharCarrinho = document.querySelector('.close');
+const itensCarrinho = document.querySelector('.carrinho-pizza');
+const Endereco = document.querySelector('.endereco');
+const Total = document.querySelector('.total');
+const finalizar = document.querySelector('.btn-finalizar');
+const Cardapio = document.querySelector('.imagens');
+const pizza = document.querySelector('.item');
 
+Cardapio.addEventListener('click', (event) => {
+    if (event.target.closest('.item')) {
+        fundomenu.style.display = 'flex';
+        let foto = event.target.closest('.item').querySelector('img').src.split('/').pop();
+        let nome = event.target.closest('.item').querySelector('p').textContent;
+        let preco = event.target.closest('.item').getAtribute('data-precogrande');
+        menu.insertAdjacentHTML('beforeend', `<div>
+                    <img src="fotos cardapio/${foto}" alt="" class="pizza-imagem">
+                </div>
+                <h2 class="pizza-nome">PIZZA: ${nome}</h2>`);
+    }
 
+});
 
-cardapio.addEventListener("click", (event) => {
-    let parant = event.target.closest(".item");
-    if (parant) {
-        const imagemPizza = parant.getAttribute("data-img");
-        const nomePizza = parant.getAttribute("data-pizza");
-        const precoPizza = parant.getAttribute("data-preco");
-
-fundoMenu.style.display = 'flex';
-
-pizza.insertAdjacentHTML("beforeend", `
-            <div class="pizza-imagem">
-            <img src="${imagemPizza}" alt="">
-            <h2 id="pizza-nome">${nomePizza}</h2>
-        </div>
-        <div class="pizza-informacoes">
-            <h3 id="pizza-preco">R$${precoPizza}</h3>
-            <p id="pizza-descricao">Pizza de chocolate com morango.</p>
-        </div>
-        `);
+fundomenu.addEventListener('click', (event) => {
+    if (event.target === fundomenu) {
+        fundomenu.style.display = 'none';
     }
 });
 
-fecharMenu.addEventListener('click', function() {
-        fundoMenu.style.display = 'none';
-    })
-    fundoMenu.addEventListener('click', function(event) {
-        if (event.target === fundoMenu) {
-            fundoMenu.style.display = 'none';
-        }
-    })
+fecharMenu.addEventListener('click', () => {
+    fundomenu.style.display = 'none';
+});
 
-addCart.addEventListener('click', function() {
-    fundoCarinho.style.display = 'flex';
-    fundoMenu.style.display = 'none';
-    fecharCarrinho.addEventListener('click', function() {
-        fundoCarinho.style.display = 'none';
-    })
-    fundoCarinho.addEventListener('click', function(event) {
-        if (event.target === fundoCarinho) {
-            fundoCarinho.style.display = 'none';
-        }
-    })
-})
+AddCart.addEventListener('click', () => {
+    FundoCarrinho.style.display = 'flex';
+    fundomenu.style.display = 'none';
+});
 
-/*function modificarPizza(){
-    pizza.innerHTML = ''; // Limpa o conteúdo anterior
-    const divpizza = document.createElement("div");
-    divpizza.insert =`
-        <div class="pizza-imagem">
-            <img src="${imagemPizza}" alt="">
-            <h2 id="pizza-nome">${nomePizza}</h2>
-        </div>
-        <div class="pizza-informacoes">
-            <h3 id="pizza-preco">R$${precoPizza}</h3>
-            <p id="pizza-descricao">Pizza de chocolate com morango.</p>
-        </div>
-        `;
-    pizza.appendChild(divpizza);
-
-}*/
-
+FundoCarrinho.addEventListener('click', (event) => {
+    if (event.target === FundoCarrinho) {
+        FundoCarrinho.style.display = 'none';
+    }
+});
+FecharCarrinho.addEventListener('click', () => {
+    FundoCarrinho.style.display = 'none';
+});
+finalizar.addEventListener('click', () => {
+    window.open(`https://wa.me/5511976393636?text=Olá! Gostaria de fazer um pedido: 
+        1 Pizza de Bolonhesa
+        Entregar em: ${Endereco.value}
+        Total: ${Total.value}`, '_blank');
+        
+    }
+);
